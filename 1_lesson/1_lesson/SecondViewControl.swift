fe0b1ffdaba047beb6227e0a3c1f2a527e760c3c
Vladimir <vovamatveyev2007@gmail.com>
2021-02-03 02:21:23 +0300

    1_lesson

---
+++ 1_lesson/1_lesson/SecondViewControl.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewControl.swift
+//  1_lesson
+//
+//  Created by Владимир Матвеев on 01.02.2021.
+//
+
+import SwiftUI
+
+struct SecondViewControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondViewControl_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondViewControl()
+    }
+}
